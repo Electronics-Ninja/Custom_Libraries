05a7fd57787b1bafd23dccafe9f3bf28760be4e0
Paul Kincaid <paul.m.kincaid@gmail.com>
2017-12-23 20:22:55 -0700

    Create MongoDB Library

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Custom_Libraries
+# Custom Libraries from Electronics Ninja
+
+You may or may not find them useful - these are really just to make my life easier
---
+++ en_mongodb.py
@@ -0,0 +1,33 @@
+import pymongo
+
+class MongoDB(object):
+    """ Electronics Ninja custom MongoDB initiation and worker function
+    :param auth: <boolean> (required) True = MongoDB auth enabled
+    :param mongodb_creds: <dict> (required if auth set)
+        :param server: <str> MongoDB server - defaults to localhost
+        :param port: <int> MongoDB server - defaults to 27017
+        :param username: <str> MongoDB username
+        :param password: <str> MongoDB user password
+        :param database: <str> Database to authenticate against and subsequently use
+    """
+    def __init__(self, auth, mongodb_creds):
+        server = mongodb_creds['server']
+        port = mongodb_creds['port']
+        username = mongodb_creds['username']
+        password = mongodb_creds['password']
+        database = mongodb_creds['database']
+
+        self.client = pymongo.MongoClient(server, port=port, username=username, password=password, auth_source=database)
+        self.db = client['{}'.format(database)]
+
+    def collection(self, coll_name):
+        """ Create or attach to a collection
+        :param coll_name: <str> (Required) Name of the collection
+
+        Returns the collection object => make sure you assign to a variable in your program
+        """
+        self.coll_name = self.db['{}'.format(coll_name)]
+        return self.coll_name
+
+    def insert_one(self, coll_name, data):
+        coll_name.insert_one(data)
